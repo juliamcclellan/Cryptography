9aa16248ab770e001ad6cade2fb5e6e60efe9bbf
juliamcclellan <jmcclellan@mxschool.edu>
2016-05-20 10:16:37 -0400

    Changes

--- RSA_Frame.java
+++ RSA_Frame.java
@@ -6,21 +6,18 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
 import javax.swing.Box;
-import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.JTextPane;
 
 public class RSA_Frame extends JFrame
 {
 	private JTextPane enter, result;
-	private JRadioButton e;
 	private JButton code;
 	private RSA_Frame other; //The person to send and receive messages from
 	
-	public RSA_Frame(String name, boolean canDecrypt, final RSA_Frame other)
+	public RSA_Frame(String name, final boolean canDecrypt, final RSA_Frame other)
 	{
 		super(name);
 		this.other = other;
@@ -30,23 +27,6 @@ public class RSA_Frame extends JFrame
 		c.gridy = 0;
 		c.anchor = GridBagConstraints.NORTH;
 		
-		e = new JRadioButton("Encrypt", true);
-		if(canDecrypt)
-		{	
-			JRadioButton d = new JRadioButton("Decrypt");
-			ButtonText l = new ButtonText();
-			e.addActionListener(l);
-			d.addActionListener(l);
-			ButtonGroup g = new ButtonGroup();
-			JPanel buttons = new JPanel();
-			g.add(e);
-			g.add(d);
-			buttons.add(e);
-			buttons.add(d);
-			panel.add(buttons, c);
-		}
-		
-		c.gridy++;
 		enter = new JTextPane();
 		enter.setPreferredSize(new Dimension(300, 100));
 		enter.setMinimumSize(new Dimension(300, 100));
@@ -54,6 +34,7 @@ public class RSA_Frame extends JFrame
 		
 		c.gridy++;
 		code = new JButton("Encrypt");
+		if(canDecrypt) code.setText("Decrypt");
 		code.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent arg0)
@@ -61,7 +42,7 @@ public class RSA_Frame extends JFrame
 				if(enter.getText().equals("")) return;
 				try
 				{
-					if(e.isSelected()) result.setText(RSA.convertToString(RSA.encrypt(enter.getText())));
+					if(!canDecrypt) result.setText(RSA.convertToString(RSA.encrypt(enter.getText())));
 					else result.setText(RSA.decrypt(RSA.convertFromString(enter.getText())));
 				}
 				catch (FileNotFoundException e1) 
@@ -104,19 +85,5 @@ public class RSA_Frame extends JFrame
 	{
 		enter.setText(m);
 		result.setText("");
-	}
-	
-	public void addUser(RSA_Frame other)
-	{
-		this.other = other;
-	}
-	
-	private class ButtonText implements ActionListener
-	{
-		public void actionPerformed(ActionEvent arg0)
-		{
-			if(e.isSelected()) code.setText("Encrypt");
-			else code.setText("Decrypt");
-		}
 	}
 }
